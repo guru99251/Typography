2867a2712352f25183e5e6ba90a2945d5a7225e7
ensemble_teri <guru99251@khu.ac.kr>
2025-04-19 17:03:50 +0900

    mod app2

--- web-typo-auto-setting/app.py
+++ web-typo-auto-setting/app.py
@@ -1,7 +1,7 @@
 from flask import Flask, send_from_directory, jsonify
 import os
 port = int(os.environ.get("PORT", 5000))
-app.run(host='0.0.0.0', port=5000)
+app.run(host='0.0.0.0', port=port)
 
 
 app = Flask(__name__)
