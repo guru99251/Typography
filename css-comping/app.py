14a9964631ca9a84267459ee6ae0f779c8fa30eb
ensemble_teri <guru99251@khu.ac.kr>
2025-04-19 17:26:58 +0900

    mod2

--- css-comping/app.py
+++ css-comping/app.py
@@ -20,5 +20,5 @@ def serve_translations():
     return send_from_directory('.', 'translations.json')
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 3000))
     app.run(host='0.0.0.0', port=port)
